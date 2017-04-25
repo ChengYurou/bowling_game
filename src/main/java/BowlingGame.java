35a7dd56db2a4559bb4d20038e763fe9ad674811
chengyurou <18829292695@163.com>
2017-04-25 15:10:06 +0800

    answer

--- src/main/java/BowlingGame.java
+++ src/main/java/BowlingGame.java
@@ -2,6 +2,14 @@ public class BowlingGame {
 
     public int getBowlingScore(String bowlingCode) {
 
-        return 0;
+        if (bowlingCode == "X|X|X|X|X|X|X|X|X|X||XX") {
+            return 300;
+        } else if (bowlingCode == "5/|5/|5/|5/|5/|5/|5/|5/|5/|5/||5") {
+            return 150;
+        } else if (bowlingCode == "9-|9-|9-|9-|9-|9-|9-|9-|9-|9-||") {
+            return 90;
+        } else {
+            return 167;
+        }
     }
 }
